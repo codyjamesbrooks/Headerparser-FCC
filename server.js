34205d44e9871ab621a0cedbc978fb3c20ab281d
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-05 10:36:58 -0600

    added res.json at /api/whoami

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
